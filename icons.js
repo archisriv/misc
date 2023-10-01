38fca2f4bc051311e79ad13245129ee44d8a33be
xumfi <96708375+xumfi@users.noreply.github.com>
2023-10-01 01:41:13 +0000

    Add files via upload

---
+++ icons.js
@@ -0,0 +1,29 @@
+import { Plugin, registerPlugin } from 'enmity/managers/plugins'
+import manifest from '../manifest.json'
+import { getByProps } from "enmity/modules"
+
+// finally figured getbyprops out
+const icons = getByProps("OFFICIAL_ALTERNATE_ICONS")
+const iconsIds = getByProps("FreemiumAppIconIds")
+const alternateIcons = icons.OFFICIAL_ALTERNATE_ICONS();
+const origFreemiumAppIconIds = iconsIds.FreemiumAppIconIds;
+
+const FreeAppIcons: Plugin = {
+    ...manifest,
+
+//  when the app starts make the icons free for all/each of them
+    onStart() {
+        alternateIcons.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = false));
+        icons.ICON.forEach(icon => (icon._isPremium = icon.isPremium, icon.isPremium = false));
+        iconsIds.FreemiumAppIconIds = iconsIds.MasterAppIconIds;
+    },
+
+// unpach it all on stop   
+    onStop() {
+       alternateIcons.forEach(icon => icon.isPremium = (icon._isPremium ?? false));
+       icons.ICON.forEach(icon => icon.isPremium = (icon._isPremium ?? false));
+       iconsIds.FreemiumAppIconIds = origFreemiumAppIconIds;
+    }
+ };
+ 
+ registerPlugin(FreeAppIcons);
