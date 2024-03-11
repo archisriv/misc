7eefabbc17952ff3e998fa729df12384516b91c2
xumfi <96708375+xumfi@users.noreply.github.com>
2024-03-11 23:10:04 +0000

    Add files via upload

---
+++ theme (3).js
@@ -0,0 +1,26 @@
+//Greeting
+ctx.font = 'bold 66px -apple-system';
+ctx.textAlign = "center";
+ctx.fillStyle = "#929999";
+ctx.shadowOffsetY = 12;
+ctx.shadowColor = "rgba(0,0,0,0.9)";
+ctx.shadowBlur = 32;
+ctx.fillText('$GREETING$', 642, 1081, 1000);
+
+//Line1
+ctx.font = 'bold 42px -apple-system';
+ctx.textAlign = "center";
+ctx.fillStyle = "#929999";
+ctx.shadowOffsetY = 10;
+ctx.shadowColor = "rgba(0,0,0,0.9)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING1$', 642, 1221, 1000);
+
+//Line2
+ctx.font = 'bold 42px -apple-system';
+ctx.textAlign = "center";
+ctx.fillStyle = "#929999";
+ctx.shadowOffsetY = 10;
+ctx.shadowColor = "rgba(0,0,0,0.9)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING2$', 642, 1276, 1000);
